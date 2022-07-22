13ae1612edcd8fba8cb0c25dbddfc31da491b2b9
João Ribeiro <joaoribeiroh1@gmail.com>
2022-07-22 12:04:35 -0300

    usandoBinding

---
+++ app/src/main/java/com/example/controleentradabinding/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.controleentradabinding
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.controleentradabinding.databinding.ActivityMainBinding
+
+private lateinit var binding : ActivityMainBinding
+private var contador: Int = 0
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.entrou.setOnClickListener {
+            contador++
+            binding.textView.text = "Pessoas presentes: $contador"
+            if (contador == 30){
+                binding.entrou.isClickable = false
+                binding.textView.text = "Casa lotada, impossivel entrar!"
+
+
+            }
+            binding.saida.isClickable = true
+        }
+
+
+        binding.saida.setOnClickListener {
+            contador--
+            binding.textView.text = "Pessoas presentes: $contador"
+            if (contador == 0){
+                binding.saida.isClickable = false
+                binding.textView.text = "Casa vazia, impossivel sair!"
+
+
+
+            }
+            binding.entrou.isClickable = true
+        }
+    }}
+
+
+
+
+
+
